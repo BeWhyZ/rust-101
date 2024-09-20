3b7085815a4bdc42cc69c3869a6d513fd19d87b1
boyan <yowhyz@gmail.com>
2024-09-20 10:52:59 +0000

    feat:fifth ptr

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,6 +1,6 @@
 // an ok unsafe queue
 
-use std::mem;
+use std::{mem, ptr};
 
 pub struct List<T> {
     head:Link<T>,
@@ -18,28 +18,24 @@ struct Node<T> {
 
 impl<T> List<T>{
     pub fn new()-> Self {
-        List { head: None, tail: None }
+        List { head: None, tail: ptr::null_mut() }
     }
 
-    pub fn push(&'a mut self, elem:T) {
-        let new_tail = Box::new(Node{
+    pub fn push(&mut self, elem:T) {
+        let mut new_tail = Box::new(Node{
             elem:elem,
             next:None
         });
-        // mem swap
-        // tail push new node
-        let new_tail = match self.tail.take(){
-            Some(old_tail)=>{
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
-            },
-            None=>{
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            },
-        };
-
-        self.tail = new_tail;
+        // How do we make a raw pointer from a normal pointer? Coercions! If a variable is declared to be a raw pointer, a normal reference will coerce into it:
+        let raw_tail: *mut _ = &mut *new_tail;
+        if !self.tail.is_null(){
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -48,7 +44,7 @@ impl<T> List<T>{
             self.head = head.next;
 
             if self.head.is_none(){
-                self.tail = None;
+                self.tail = ptr::null_mut();
             }
 
             head.elem
@@ -64,11 +60,38 @@ mod tests{
     #[test]
     fn basic(){
         let mut list = List::new();
-        
-        list.push(1);list.push(2);list.push(3);
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
         assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
